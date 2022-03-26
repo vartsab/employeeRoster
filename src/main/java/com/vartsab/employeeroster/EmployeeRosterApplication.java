aac32c96296f1e1150e778269f603840a55af354
varstab <dan.vartsab@gmail.com>
2022-03-26 12:48:15 -0400

    Initial commit

---
+++ src/main/java/com/vartsab/employeeroster/EmployeeRosterApplication.java
@@ -0,0 +1,13 @@
+package com.vartsab.employeeroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRosterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRosterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vartsab/employeeroster/EmployeeRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vartsab.employeeroster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRosterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
