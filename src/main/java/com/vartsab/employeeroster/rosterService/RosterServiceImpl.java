9e8cdce78522c806fc045f3a893ae444921f775a
varstab <dan.vartsab@gmail.com>
2022-03-26 22:37:41 -0400

    commit

--- src/main/java/com/vartsab/employeeroster/rosterService/RosterServiceImpl.java
+++ src/main/java/com/vartsab/employeeroster/rosterService/RosterServiceImpl.java
@@ -5,6 +5,7 @@ import com.vartsab.employeeroster.exceptions.exceptions.BadRequest;
 import com.vartsab.employeeroster.exceptions.exceptions.InternalSrvErr;
 import com.vartsab.employeeroster.exceptions.exceptions.NotFound;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
 
 @Service
 public class RosterServiceImpl implements RosterService {
@@ -13,43 +14,57 @@ public class RosterServiceImpl implements RosterService {
     private int rosterLength = 0;
     private final Employee[] employeeRoster = new Employee[maxRosterLen];
 
+    private int searchEmployee(String firstName, String lastName) {
+        Employee employee = new Employee(firstName, lastName);
+        for (int i = 0; i < rosterLength; i++) {
+            if (employeeRoster[i].equals(employee))
+                return i;
+        }
+        return -1;
+    }
+
+    private String addItem(String firstName, String lastName) {
+        Employee newEmployee = new Employee(firstName, lastName);
+        employeeRoster[rosterLength++] = newEmployee;
+        return newEmployee.toString();
+    }
+
+    private void removeItem(int i) {
+        while (i < (--rosterLength)) {
+            employeeRoster[i] = employeeRoster[i + 1];
+            i++;
+        }
+        employeeRoster[rosterLength] = null;
+    }
+
     public String findEmployee ( String firstName, String lastName ) {
-        Employee employee2 = new Employee(firstName, lastName);
-        for (int i = 0; i < rosterLength; i++)
-            if (employeeRoster[i].equals(employee2))
-                return employeeRoster[i].toString();
-        System.out.println("Error 404");
-        throw new NotFound();
+        int i = searchEmployee(firstName, lastName);
+        if ( i == -1) {
+            System.out.println("Error 404. Not found.");
+            throw new NotFound();
+        } else
+            return employeeRoster[i].toString();
     }
 
     public String addEmployee ( String firstName, String lastName ) {
         try {
             findEmployee(firstName, lastName);
-        } catch (RuntimeException e) {
+        } catch (NotFound e) {
             if ( rosterLength >= maxRosterLen ) {
-                System.out.println("Error 500");
+                System.out.println("Error 500. Roster is full.");
                 throw new InternalSrvErr();
             }
-            Employee newEmployee = new Employee(firstName, lastName);
-            employeeRoster[rosterLength++] = newEmployee;
-            return newEmployee.toString();
+            return addItem(firstName, lastName);
         }
-        System.out.println("Error 400");
+        System.out.println("Error 400. Already exists.");
         throw new BadRequest();
     }
 
     public String removeEmployee ( String firstName, String lastName ) {
-        Employee employee2 = new Employee(firstName, lastName);
-        for (int i = 0; i < rosterLength; i++)
-            if (employeeRoster[i].equals(employee2)) {
-                while (i < (rosterLength-1)) {
-                    employeeRoster[i] = employeeRoster[i + 1];
-                    i++;
-                }
-                employeeRoster[--rosterLength] = null;
-                return employee2.toString();
-            }
-        return employee2 + " not found!";
+        Employee employee = new Employee(firstName, lastName);
+        int i = searchEmployee(firstName, lastName);
+        removeItem(i);
+        return employee.toString();
     }
 
 }
